78b2bba5df9a4241dfaf14e2649593e8f69555c5
Neal Feierabend <neal@nealf.com>
2016-08-20 14:05:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This creates a proxy that can be used by a modified version of click_that_hood.
---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var request = require('request');
+var app = express();
+
+app.get('/v3/*', function(req, res) {
+  var callback = req.param('callback');
+  res.send(callback + '({"attribution":"<a href=\\"https://www.mapbox.com/about/maps/\\" target=\\"_blank\\">&copy; Mapbox</a> <a href=\\"http://www.openstreetmap.org/about/\\" target=\\"_blank\\">&copy; OpenStreetMap</a> <a class=\\"mapbox-improve-map\\"  href=\\"https://www.mapbox.com/map-feedback/\\" target=\\"_blank\\">Improve this map</a> <a href=\\"https://www.digitalglobe.com/\\" target=\\"_blank\\">&copy; DigitalGlobe</a>","autoscale":true,"bounds":[-180,-85.0511,180,85.0511],"center":[0,0,3],"created":1362777999636,"data":["http://a.tiles.mapbox.com/v3/codeforamerica.map-mx0iqvk2/markers.geojsonp"],"dataset":"d731e37d666fc36817bbc9ad0a403d16","description":"The map used by Click That Hood (http://www.click-that-hood.com)","embed":"http://a.tiles.mapbox.com/v3/codeforamerica.map-mx0iqvk2.html","geocoder":"http://a.tiles.mapbox.com/v3/codeforamerica.map-mx0iqvk2/geocode/{query}.jsonp","id":"codeforamerica.map-mx0iqvk2","maxzoom":22,"minzoom":0,"name":"Click That Hood","private":false,"scheme":"xyz","tilejson":"2.0.0","tiles":["http://localhost:8081/tiles/codeforamerica.map-mx0iqvk2/{z}/{x}/{y}.png","http://localhost:8081/tiles/codeforamerica.map-mx0iqvk2/{z}/{x}/{y}.png"],"webpage":"http://a.tiles.mapbox.com/v3/codeforamerica.map-mx0iqvk2/page.html"});'
+  )
+});
+app.get('/tiles/*', function (req, res) {
+  var path = req.originalUrl.split("/");
+  var y = path.pop().split('.')[0];
+  var x = path.pop();
+  var z = path.pop();
+  //z = 16;
+  var image_url = "https://api.mapbox.com/styles/v1/nealf/cirtivc6j000lg9kw0ew99cdx/tiles/256/"+z+"/"+x+"/"+y+"?access_token=pk.eyJ1IjoibmVhbGYiLCJhIjoiNmM4MGQ3M2UzNmVlMTY0OWNmZDhiZjk0YWZlYzQ4OTYifQ.VEiV66Tl7sjD5n-bDLjbhw";
+  request.get(image_url).pipe(res);
+});
+
+app.listen(3000, function () {
+  console.log('Proxy listening on port 3000!');
+});
